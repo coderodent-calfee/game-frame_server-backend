d4f57189cc9b22a38dc59d77aa6bb322236c96f4
coderodent-calfee <robert.w.calfee@gmail.com>
2025-09-26 10:43:34 -0700

    server runs under https

--- start.bat
+++ start.bat
@@ -3,6 +3,5 @@ cd server_backend
 
 :: Start Daphne using Pipenv
 
-rem start "Game-Frame Backend Server" 
-pipenv run daphne -p 8000 -b 0.0.0.0 server_backend.asgi:application
-rem caddy run
+start "Game-Frame Backend Server" pipenv run daphne -p 8000 -b 0.0.0.0 server_backend.asgi:application
+caddy run
